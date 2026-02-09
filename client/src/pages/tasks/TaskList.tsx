86c035ae14572c4bcd30e07ed32141302aa68352
jinfei <jinfei@supermap.com>
2026-02-09 17:27:27 +0800

    添加更多日志信息，用于调试定时任务执行问题

--- client/src/pages/tasks/TaskList.tsx
+++ client/src/pages/tasks/TaskList.tsx
@@ -4,8 +4,6 @@ import { ReloadOutlined, PlusOutlined, SearchOutlined, PlayCircleOutlined, EditO
 import { useNavigate } from 'react-router-dom';
 import type { ColumnsType } from 'antd/es/table';
 import { apiRequest } from '../../config/api';
-// 导入cron-parser
-
 
 interface Task {
   id: number;
